68009be74f41dd88d305e5d14565948d1ab1d572
Brendt Bly <brendtbly@Brendts-MacBook-Pro.local>
2017-03-24 13:24:34 -0500

    Done but tests won't pass

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -6,6 +6,21 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+/*
+ (1) - In the BankVault.swift file, create a protocol named ProvideAccess. This protocol should require any class/struct that conforms to it to implement a function called allowEntryWithPassword which takes in one argument called password of type [Int] (an array of Ints). The function should return a Bool.
+ 
+ (2) - In the BankVault.swift file, below where you created the ProvideAccess protocol - create an extension on the BankVault class. In that extension, it should conform to the ProvideAccess protocol. In your implementation of the allowEntryWithPassword(_:) function, you should adhere to the following rules:
+ 
+ If the person calling on this function doesn't enter in any digits (empty array), then we should return false.
+ If the total numbers of digits entered (items in the array) exceed 10, then we should return false.
+ In order to allow access (return true), every other digit (starting with the first digit) must be even. The first digit entered is the first element in the array.
+ */
+
+protocol ProvideAccess {
+    func allowEntryWithPassword(password: [Int]) -> Bool
+}
+
+
 
 class BankVault {
     
@@ -20,7 +35,16 @@ class BankVault {
     
 }
 
-
-
-
-
+extension BankVault {
+    func allowEntryWithPassword(password: [Int]) -> Bool {
+        guard !password.isEmpty else {return false}
+        guard password.count <= 10 else {return false}
+        for i in 0...5 {
+            let index = i * 2
+            if password[index] % 2 != 0 {
+                return false
+            }
+        }
+        return true
+    }
+}
